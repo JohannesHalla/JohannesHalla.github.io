c43717071140b0aa163680e3975e00ea437052e3
johannes <johannes@192.168.0.80>
2018-03-29 13:36:56 +0200

    Aufgabe verschoben

--- Aufgabe0/Aufgabe0.ts
+++
@@ -1,7 +0,0 @@
-function Feld() {
-  var eingabe = prompt("Wie lautet Ihr Name?", "");
-  if (eingabe != null) {
-    document.getElementById("HTML").innerHTML =
-      "Herzlich Willkommen " + eingabe + "!";
-  }
-}
